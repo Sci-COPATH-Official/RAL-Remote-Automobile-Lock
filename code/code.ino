347a955ef398152183eef3f12dec552dfa50ed15
Sci-COPATH <scicopath01@gmail.com>
2021-07-25 19:17:19 +0530

    Add code

--- code/code.ino
+++ code/code.ino
@@ -1,5 +1,13 @@
 class RAL{
+    private:
+        int relay;
+        void relayAttach(int Relay){
+            relay=Relay;
+            pinMode(relay,OUTPUT);
+        }
+        void SendMsg(String msg){
 
+        }
 };
 void setup(){
 
